bb9b59c748cfd6e1e9a04492c753e937070cf021
alvarogularte <alvaro.gularte@gmail.com>
2022-03-17 15:51:07 -0300

    Adicionando um h2

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
+app.get('/', (req, res) => res.send(`<h2> Avengers of Lost Ark </h2>`));
 app.listen(port, () => console.log(`Online: ${port}`));
